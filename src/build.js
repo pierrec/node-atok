9a49e8ad438ee08e6a18811e4ee008b40a2c4774
Pierre Curto <pierre.curto@gmail.com>
2012-10-11 15:11:41 +0200

    Removed test method building

--- src/build.js
+++ src/build.js
@@ -1,61 +1,7 @@
-var fs = require('fs')
 var path = require('path')
 var exec = require('child_process').exec
 
 var fstream = require('fstream')
-
-// Define the Rule#test method as a masked method
-/*
-  for each flag, generate a unique method based on a shared code
-  -> optimize the method based on user defined flags (avoid unnecessary if()s)
-  -> only useful in highly demanding scenarii
-**/
-var methodName = 'test'
-var vars = 'RULE_GENERATES_TOKEN RULE_TRIMLEFT RULE_TRIMRIGHT DEBUG'.split(' ')
-var data = []
-  , n = vars.length
-  , res = new Array(32) // 32bits integer -> array of its bits
-
-// Masked method setter:
-// call this function to set the right method based on incoming flags
-data.push(
-  '// The content of this file was automatically generated during build time'
-, 'function _MaskSetter (method /* , flag1, flag2... */) {'
-// Convert the list of flags to an integer
-, '  for (var list = new Array(32), i = 0; i < 32; i++) {'
-, '    list[i] = arguments[32 - i] ? 1 : 0'
-, '  }'
-, '  this[method] = this[ method + "_" + parseInt(list.join(""), 2) ]'
-, '}'
-, ''
-)
-
-// Number of possible cases: 2^n, where n is the number of boolean variables
-for (var i = 0, num = Math.pow(2, n); i < num; i++) {
-  // Convert the current number into an array of bits
-  for (var int = i, j = 0; j < 32; j++) {
-    res[j] = int & 1
-    int = int >> 1
-  }
-
-  var _vars = vars
-      .map(function (vname, idx) { return vname + '=' + res[idx] })
-      .join(',')
-  // Add the list of variables and the associated method
-  data.push(
-    '// ' + _vars
-  , '//var ' + _vars
-  , 'Rule.prototype.' + methodName + '_' + i + ' = '
-  + '//include("rule#' + methodName + '.js")'
-  )
-}
-data.push('')
-
-// Save the methods to a file
-fs.writeFileSync(
-  path.join( __dirname, 'string/rule#' + methodName + '_masked.js' )
-, data.join('\n')
-)
 
 build({
   "input": {
